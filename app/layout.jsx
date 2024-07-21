b76a4b2472d914cb3876bee70bb02442dee344c0
aliyazakirova <93845260+aliyazakirova@users.noreply.github.com>
2024-07-22 00:36:00 +0300

    layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,26 +1,21 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "TrailHive",
   description: "Your Ultimate Outdoor Companion",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
+        <title>{metadata.title || "Default Title"}</title>
+        <meta name="description" content={metadata.description || "Default Description"} />
       </head>
       <body>
         <Navbar />
